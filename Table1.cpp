658228ae6975ab8602340ff2d7765bb0a89d9539
shahzaibbukhari-1 <shahzaibbukhari158@gmail.com>
2025-11-26 20:15:18 +0500

    Add files via upload

---
+++ Table1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "+-----------------------+\n";
+	cout << "| Roll No\t| Marks |\n";
+	cout << "+-----------------------+\n";
+	cout << "| 101\t\t| 89    |\n";
+	cout << "| 102\t\t| 92    |\n";
+	cout << "| 103\t\t| 85    |\n";                   
+	cout << "+-----------------------+\n";
+	return 0;
+}
