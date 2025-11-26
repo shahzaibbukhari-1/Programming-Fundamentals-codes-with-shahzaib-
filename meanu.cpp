a40b4f8d504744d7c2c9678ae8aca98aa421ea91
shahzaibbukhari-1 <shahzaibbukhari158@gmail.com>
2025-11-26 20:17:22 +0500

    Add files via upload

---
+++ meanu.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout <<"**************Menu**************\n";
+	cout <<"1.\tBiryani\t\tRs.  350\n";
+	cout <<"2.\tKarahi\t\tRs.  600\n";
+	cout <<"3.\tQorma\t\tRs.  500\n";
+	cout <<"4.\tDrinks\t\tRs.  100\n";
+	cout <<"***********************************\n";
+	return 0;
+}
