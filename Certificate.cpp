6d42fcdd5cc9f9dfaa1e0b9f0db529e0f085ab75
shahzaibbukhari-1 <shahzaibbukhari158@gmail.com>
2025-11-26 13:05:11 +0500

    Add files via upload

---
+++ Certificate.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout<<"--------------------------------------------\n";
+cout<<"\tCertificate of Achievement\n";
+cout<<"--------------------------------------------\n";
+cout<<"This is to certify that \"Ali Raza\"\n";
+cout<<"has successfully completed the course\n";
+cout<<" \"c++ programing\"with A+ Grade.\n";
+cout<<"--------------------------------------------\n";
+return 0;
+}
