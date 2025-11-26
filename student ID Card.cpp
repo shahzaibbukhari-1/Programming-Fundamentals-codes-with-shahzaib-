0c67900fefaed2026a04524b14fd8abcc200878e
shahzaibbukhari-1 <shahzaibbukhari158@gmail.com>
2025-11-26 20:18:55 +0500

    Add files via upload

---
+++ student ID Card.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"----------------------------------------------------\n";
+	cout<< "\tstudent ID Card\n";
+	cout<<"----------------------------------------------------\n";
+	cout<< "Name:\t\tshahzaib\n";
+	cout<< "Roll No:\t45\n";
+	cout<< "Deperment:\tcomputer science\n";
+	cout<< "Session:\t2025-2029\n";
+	cout<<"----------------------------------------------------\n";
+	return 0;
+	
+}
